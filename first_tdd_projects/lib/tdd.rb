aae4fa7bcda99aac8f15b267d1fbcfadd23fd032
Michael Chen <michael.chen.1120@gmail.com>
2022-04-21 11:07:02 -0400

    Add two sum

--- first_tdd_projects/lib/tdd.rb
+++ first_tdd_projects/lib/tdd.rb
@@ -7,9 +7,17 @@ class Array
     end
 
     def two_sum
-        
+        result = []
+        (0...self.length-1).each do |i|
+            (i+1...self.length).each do |j|
+                result << [i,j] if self[i] + self[j] == 0
+            end
+        end
+        result
     end
 
 end
 
 p [1, 2, 1, 3, 3].my_uniq # => [1, 2, 3]
+
+p [-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
