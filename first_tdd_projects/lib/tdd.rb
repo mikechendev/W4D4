bb9906553276c17cb01696502532161df620819f
Leo Chung <leo.chung97@gmail.com>
2022-04-21 14:01:45 -0400

    TDD Part 1

--- first_tdd_projects/lib/tdd.rb
+++ first_tdd_projects/lib/tdd.rb
@@ -16,11 +16,44 @@ class Array
         num_index[num] << idx
       end
     end
-    pairs
+    pairs.reverse
   end
 end
 
+def my_transpose(arr)
+  transpose = Array.new(arr.length) {Array.new(arr.length)}
+  (0...arr.length).each do |i|
+    (0...arr.length).each do |j|
+      transpose[j][i] = arr[i][j]
+    end
+  end
+  transpose
+end
 
-p [1, 2, 1, 3, 3].my_uniq # => [1, 2, 3]
+def stock_picker(arr)
+  max_profit = 0
+  pairs = []
+  lowest = arr.first
+  arr.each_with_index do |num, idx|
+    lowest = num if num < lowest
+    if num - lowest > max_profit
+      pairs = [arr.index(lowest), idx]
+    end
+  end
+  pairs
+end
 
-p [-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
+# p [1, 2, 1, 3, 3].my_uniq # => [1, 2, 3]
+
+# p [-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
+
+# p my_transpose(
+#     [[0, 1, 2],
+#     [3, 4, 5],
+#     [6, 7, 8]
+#   ])
+#  # => [[0, 3, 6],
+#  #    [1, 4, 7],
+#  #    [2, 5, 8]]
+
+#  p stock_picker([7,1,5,3,6,4]) # => [1, 5]
--- first_tdd_projects/spec/tdd_spec.rb
+++ first_tdd_projects/spec/tdd_spec.rb
@@ -2,9 +2,40 @@ require "tdd.rb"
 require "rspec"
 
 describe Array do
-  context "Two_Sum" do
+  let(:array) { [1, 2, 1, 3, 3] }
+
+  describe "my_uniq" do
+    it "finds unique values correctly" do
+      expect(array.my_uniq).to eq([1, 2, 3])
+    end
+  end
+  
+  describe "two_sum" do
     it "finds sum pairs correctly" do
       expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
     end
+  end
+end
+
+describe "my_transpose" do
+  it "transposes a matrix correctly" do
+    matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
+
+    expect(my_transpose(matrix)).to eq([
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9]
+    ])
+  end
+end
+
+describe "stock_picker" do
+  let(:days) {[7,1,5,3,6,4]}
+  it "should pick the right days to buy and sell" do
+    expect(stock_picker(days)).to eq([1,5])
   end
 end
